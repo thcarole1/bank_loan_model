e12f8c37aad38ac19d16f1cc5593293e0596a7af
Thierry CAROLE <thcarole1@hotmail.com>
2024-12-30 19:55:31 +0100

    20241230_1955_backup

--- bank_loan_model_package_folder/main.py
+++ bank_loan_model_package_folder/main.py
@@ -1,3 +1,23 @@
+
+
+def predict_house_price(number):
+    # Retrieve housing data
+    housing = load_housing_data()
+
+    # Extract a specific number of rows
+    housing_sample = housing.sample(number)
+
+    # Create X (features) and y (target)
+    X, y = create_x_y(housing_sample)
+
+    # Get the model to use
+    final_model = get_model()
+
+    # Predict
+    predictions = final_model.predict(X)
+    predictions = [round(prediction, 2) for prediction in predictions]
+    print(predictions)
+
 def say_hello():
     print('Hello World !')
 
